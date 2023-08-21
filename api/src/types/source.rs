ddab901c0ca078c0f7d9aa7632b3a139436eb758
sim04ful <sim04ful@gmail.com>
2023-08-22 00:48:54 +0100

    pdf and html sources work

--- api/src/types/source.rs
+++ api/src/types/source.rs
@@ -1,10 +1,10 @@
+use crate::db::DB;
 use axum::http::HeaderValue;
 use eyre::Result;
+use scraper::{Html, Selector};
 use serde::{Deserialize, Serialize};
 use serde_aux::prelude::deserialize_number_from_string;
 use url_serde::SerdeUrl;
-
-use crate::db::DB;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct SourceInput {
@@ -29,6 +29,7 @@ pub struct Source {
     pub created_at: u32,
 }
 
+#[derive(Debug)]
 enum RemoteSourceType {
     Pdf,
     Html,
@@ -55,6 +56,21 @@ impl Source {
             RemoteSourceType::Pdf => {
                 let content_bytes = resp.bytes().await?;
                 pdf_extract::extract_text_from_mem(&content_bytes)?
+            }
+            RemoteSourceType::Html => {
+                let mut content: String = Default::default();
+
+                let selector_str: &str =
+                    "h1, h2, h3, h4, h5, h6, p, a, span, div, li, ul, ol, blockquote, pre, code";
+
+                let selector =
+                    Selector::parse(selector_str).map_err(|_| eyre::eyre!("Invalid selector"))?;
+                let body = resp.text().await?;
+                let document = Html::parse_document(&body);
+                for element in document.select(&selector) {
+                    content.push_str(element.text().collect::<Vec<_>>().join("\n").as_str());
+                }
+                content
             }
             _ => resp.text().await?,
         };
@@ -107,6 +123,7 @@ impl Source {
             _ => {
                 let cached_source =
                     Source::by_url(input_url.as_str())?.filter(|source| !source.is_expired());
+
                 match cached_source {
                     Some(source) => source,
                     _ => {
@@ -136,17 +153,7 @@ impl Source {
     }
 
     fn is_local_url(url: &str) -> bool {
-        // Extract the host part of the URL
-
-        let host_start = url.find("://").map(|pos| pos + 3).unwrap_or(0);
-        let host_end = url[host_start..]
-            .find('/')
-            .map(|pos| pos + host_start)
-            .unwrap_or_else(|| url.len());
-        let host = &url[host_start..host_end];
-
-        // Check if the host is "localhost" or "127.0.0.1"
-        host == "localhost" || host.starts_with("127.0.0.1")
+        url.contains("localhost") || url.contains("127.0.0.1")
     }
 }
 
@@ -155,12 +162,14 @@ impl From<Option<&HeaderValue>> for RemoteSourceType {
         match header {
             Some(header) => {
                 let header = header.to_str().unwrap_or("");
-                match header {
-                    "application/pdf" => Self::Pdf,
-                    "text/html" => Self::Html,
-                    "application/json" => Self::Json,
-                    "text/plain" => Self::Text,
-                    _ => Self::Text,
+                if header.contains("pdf") {
+                    Self::Pdf
+                } else if header.contains("html") {
+                    Self::Html
+                } else if header.contains("json") {
+                    Self::Json
+                } else {
+                    Self::Text
                 }
             }
             None => Self::Text,
@@ -171,3 +180,78 @@ impl From<Option<&HeaderValue>> for RemoteSourceType {
 // pub trait Source {
 //     fn content(&self) -> String;
 // }
+
+#[cfg(test)]
+mod tests {
+    use url_serde::De;
+
+    use super::*;
+
+    #[test]
+    fn local_url() {
+        assert!(Source::is_local_url("http://localhost:3000"));
+        assert!(Source::is_local_url("localhost:3000"));
+        assert!(Source::is_local_url("127.0.0.1"));
+        assert!(Source::is_local_url("http://127.0.0.1:3000"));
+    }
+
+    #[tokio::test]
+    async fn source_errors() {
+        let input = SourceInput {
+            content: None,
+            url: None,
+            expires: 86400,
+        };
+        assert!(Source::new(input).await.is_err());
+    }
+
+    #[tokio::test]
+    async fn with_content() {
+        let input = SourceInput {
+            content: Some("Hello world".to_string()),
+            url: None,
+            expires: 86400,
+        };
+        assert!(Source::new(input).await.is_ok());
+    }
+
+    #[tokio::test]
+    async fn with_url() {
+        let google_url: SerdeUrl = serde_json::from_str("\"https://google.com\"").expect("url");
+        let input = SourceInput {
+            content: None,
+            url: Some(google_url),
+            expires: 86400,
+        };
+        assert!(Source::new(input).await.is_ok());
+    }
+
+    #[tokio::test]
+    async fn html_content_should_be_parsed() {
+        let nextui_url: SerdeUrl =
+            serde_json::from_str("\"https://nextui.org/docs/getting-started\"").expect("url");
+        // let google_url: SerdeUrl = serde_json::from_str("\"https://google.com\"").expect("url");
+        let input = SourceInput {
+            content: None,
+            url: Some(nextui_url),
+            expires: 86400,
+        };
+        let (source, _) = Source::new(input).await.expect("source");
+
+        assert!(!source.content.contains("<html"));
+    }
+
+    #[tokio::test]
+    async fn pdf_content_should_be_parsed() {
+        let w3 = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
+        let pdf_url: SerdeUrl = serde_json::from_str(format!("\"{}\"", w3).as_str()).expect("url");
+        let input = SourceInput {
+            content: None,
+            url: Some(pdf_url),
+            expires: 86400,
+        };
+        let (source, _) = Source::new(input).await.expect("source");
+        println!("{:?}", source);
+        assert!(source.content.contains("Dummy"));
+    }
+}
