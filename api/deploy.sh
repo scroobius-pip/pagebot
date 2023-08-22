d688bea865cce8a7e1d013d2837bad58ce5e2ac1
sim04ful <sim04ful@gmail.com>
2023-08-22 01:06:43 +0100

    pagebotapi:pagebot

--- api/deploy.sh
+++ api/deploy.sh
@@ -3,4 +3,4 @@ cargo build --release &&
 supervisorctl stop pagebotapi:pagebot &&
 rm /home/alwyzon/pagebotapi_bin
 cp /home/alwyzon/pagebot/api/target/release/pagebotapi /home/alwyzon/pagebotapi_bin
-supervisorctl start pagebotapi
+supervisorctl start pagebotapi:pagebot
