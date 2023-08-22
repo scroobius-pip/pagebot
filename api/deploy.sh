4acbf3b79313786c5c67c52c3efa94b6b4ed6f1e
sim04ful <sim04ful@gmail.com>
2023-08-22 01:01:54 +0100

    updated source added qdrant client

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,6 +1,6 @@
 git pull &&
 cargo build --release &&
-supervisorctl stop pagebotapi &&
+supervisorctl stop pagebotapi:pagebot &&
 rm /home/alwyzon/pagebotapi_bin
 cp /home/alwyzon/pagebot/api/target/release/pagebotapi /home/alwyzon/pagebotapi_bin
 supervisorctl start pagebotapi
