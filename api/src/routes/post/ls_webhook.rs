b29b709eac8d089a94eae410e5535a39e2062198
sim04ful <sim04ful@gmail.com>
2023-10-19 21:42:52 +0100

    updated ls_webhook

--- api/src/routes/post/ls_webhook.rs
+++ api/src/routes/post/ls_webhook.rs
@@ -30,12 +30,21 @@ pub async fn main(Json(event): Json<EventData>) -> GenericResponse<()> {
         ..
     } = event;
 
-    if let Ok(Some(mut user)) = User::by_email(&user_email) {
-        user.subscribed = matches!(status, SubscriptionAttributesStatus::Active);
-        user.disabled = false;
-        user.ls_subscription_id = Some(subscription_id);
-        user.current_limit = 10_000;
-        _ = user.save();
+    if let Ok(Some(user)) = User::by_email(&user_email) {
+        let is_active = matches!(status, SubscriptionAttributesStatus::Active);
+
+        _ = User {
+            subscribed: is_active,
+            disabled: !is_active,
+            ls_subscription_id: if is_active {
+                Some(subscription_id)
+            } else {
+                None
+            },
+            current_limit: if is_active { 10_000 } else { 50 },
+            ..user
+        }
+        .save();
     }
 
     Ok((StatusCode::OK, ()))
